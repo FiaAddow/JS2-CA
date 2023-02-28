2b3a0405d872e692820d03a2d98ccedcda5f4e11
FiaAddow <100524325+FiaAddow@users.noreply.github.com>
2023-02-28 12:22:22 +0100

    correcting errors

--- src/js/api/Handlers/Register.mjs
+++ src/js/api/Handlers/Register.mjs
@@ -1,9 +1,18 @@
-const form = document.querySelector("#registerForm");
+/*const form = document.querySelector("#registerForm");
 
 form.addEventListener ("submit", (event) => {
     const form = event.target;
     const formData = new FormData(form);
     const profile = Object.fromEntries(formData.entries())
     console.log(profile)
-})
+})*/
  
+
+const form = document.querySelector("#registerForm");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const profile = Object.fromEntries(Array.from(formData.entries()));
+  console.log(profile);
+});
