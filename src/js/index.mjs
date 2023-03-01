bb347db48b0dfb2dc265272dbc7e1a35c0e15147
FiaAddow <100524325+FiaAddow@users.noreply.github.com>
2023-03-01 17:17:46 +0100

    new index

---
+++ src/js/index.mjs
@@ -0,0 +1,7 @@
+//import * as constants from "./api/handlers/constants.mjs";
+
+//console.log(constants.API_SOCIAL_URL);
+
+import { setRegisterFormListener} from "./Handlers/Register.mjs"
+
+setRegisterFormListener()
