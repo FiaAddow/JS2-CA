6a8877578003ea318bce1ee4f69bd41bf58b3de3
FiaAddow <100524325+FiaAddow@users.noreply.github.com>
2023-03-01 17:17:27 +0100

    xport function setRegisterFormListener () {}----I CANT EXPORT THIS FUNCTION-alt skal ligge inne  denne funksjonen.

--- src/js/Handlers/Register.mjs
+++ src/js/Handlers/Register.mjs
@@ -8,11 +8,15 @@ form.addEventListener ("submit", (event) => {
 })*/
  
 
-const form = document.querySelector("#registerForm");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(form);
-  const profile = Object.fromEntries(Array.from(formData.entries()));
-  console.log(profile);
-});
+
+//export function setRegisterFormListener () {}----I CANT EXPORT THIS FUNCTION-alt skal ligge inne  denne funksjonen.
+  const form = document.querySelector("#registerForm");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const profile = Object.fromEntries(formData.entries())
+    console.log('it works')
+  })
+
