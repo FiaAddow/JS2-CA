b6a956b56350efa473690670671dc5c8dee5bd94
FiaAddow <100524325+FiaAddow@users.noreply.github.com>
2023-03-02 19:00:28 +0100

    //export function setRegisterFormListener=then it works

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -1,3 +1,12 @@
-//export async function Register(profile) {
 
-//}
+import { API_SOCIAL_URL } from "../constants.mjs";
+
+const action = "/auth/register";
+const method ="post";
+
+export async function Register(profile) {
+console.log(action);
+const registerURL = API_SOCIAL_URL + action;
+console.log(registerURL);
+
+}
