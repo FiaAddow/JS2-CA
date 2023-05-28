525e2276592b64ea977cba6d34f6b64e0d647bc0
FiaAddow <farhia.dahir.addow@gmail.com>
2023-05-28 20:36:59 +0200

    fixed the edit profile button-delite still broke

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -17,4 +17,40 @@ export async function register(profile) {
 
   const results = await response.json();
   console.log(results);
+
+  if (response.ok) {
+   
+    const loginButton = document.createElement("button");
+    loginButton.textContent = "Log In";
+    loginButton.className = "bw-40 btn btn-outline-dark btn-rounded"; 
+    loginButton.setAttribute("data-mdb-ripple-color", "dark"); 
+
+ 
+    loginButton.style.marginTop = "10px";
+    loginButton.style.fontWeight = "bold";
+
+    loginButton.addEventListener("click", () => {
+  
+      window.location.href = "/profile/login.html";
+    });
+
+    const successMessage = document.createElement("p");
+    successMessage.textContent = "YEEY! You did it! Please log in!!";
+
+    const container = document.getElementById("registrationContainer");
+    container.innerHTML = ""; 
+
+    const contentWrapper = document.createElement("div");
+    contentWrapper.style.display = "flex";
+    contentWrapper.style.flexDirection = "column";
+    contentWrapper.style.alignItems = "center";
+
+    container.appendChild(successMessage);
+    container.appendChild(loginButton);
+
+    container.appendChild(contentWrapper);
+  } else {
+   
+    alert("Something went wrong. Try again!");
+  }
 }
